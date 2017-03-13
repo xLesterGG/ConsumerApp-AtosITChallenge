ab7cb62cc0994e161f318dc954d54e73f48f0ccb
Cheah Hong Loo <Cheah Hong Loo>
2017-03-13 09:55:47 +0800

    'dlFunc'

--- app/src/main/java/com/example/user/consumerapp/MainActivity.java
+++ app/src/main/java/com/example/user/consumerapp/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.user.consumerapp;
 
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -15,10 +18,18 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.net.URLConnection;
 import java.security.PublicKey;
 
 public class MainActivity extends AppCompatActivity {
+
+    String[] filePath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +37,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         RequestQueue queue = Volley.newRequestQueue(this);
+        DownloadFile dl = new DownloadFile();
+        dl.execute();
 
         String accNum = "NXT-2N9Y-MQ6D-WAAS-G88VH";
         final String url =  "http://174.140.168.136:6876/nxt?=%2Fnxt&requestType=getBlockchainTransactions&account=" + accNum;
@@ -163,5 +176,80 @@ public class MainActivity extends AppCompatActivity {
 
 
         queue.add(getRequest);
+    }
+
+    class DownloadFile extends AsyncTask<String[],String,String[]> //params,progress,result
+    {
+        ProgressDialog loading;
+        String FILE_URL="https://www.dropbox.com/s/lwyu892bezx3pb4/Btu.pem?raw=1";
+        String FILE_URL2="https://www.dropbox.com/s/4ucg8810dmhzwij/Kch.pem?raw=1";
+        String FILE_URL3="https://www.dropbox.com/s/yqucpkhhh4tw7jk/Mri.pem?raw=1";
+        String FILE_Name[]= {"BTU200"};
+        String temp;
+
+        @Override
+        protected void onPreExecute() {
+
+            super.onPreExecute();
+            loading = ProgressDialog.show(MainActivity.this,"Downloading...","Wait...", true, true);
+        }
+
+        @Override
+        protected void onPostExecute(String[] result) {
+            filePath=result;
+            loading.dismiss();
+            if(result!=null && result.length>0){
+                Toast.makeText(getApplicationContext(), "Download/Get Certs successfully", Toast.LENGTH_LONG).show();
+            }else{
+                Toast.makeText(getApplicationContext(), "No Certs", Toast.LENGTH_LONG).show();
+            }
+        }
+
+        @Override
+        protected String[] doInBackground(String[]... file_names) {
+            int readBytes;
+            String [] path = new String[FILE_Name.length];
+
+            for (int i=0;i<FILE_Name.length;i++) {
+                temp = MainActivity.this.getFilesDir() + "/" + FILE_Name[i] + ".c";
+                temp.replaceAll("\\s", " ");
+                File f = new File(temp);
+                if (f.exists()) {
+                    Log.d("fileexist", "yes");
+                    path[i] = f.toString();
+                } else {
+                    try {
+                        URL url = new URL(FILE_URL);
+                        URLConnection connection = url.openConnection();
+
+                        long fileLength = connection.getContentLength();
+                        InputStream input = new BufferedInputStream(url.openStream(), 10 * 1024);
+
+                        OutputStream output = new FileOutputStream(MainActivity.this.getFilesDir() + "/" + FILE_Name[i] + ".c");
+                        byte data[] = new byte[1024];
+                        long totalBytes = 0;
+
+                        while ((readBytes = input.read(data)) != -1) {
+                            totalBytes = totalBytes + readBytes;
+                            Long percentage = (totalBytes * 100) / fileLength;
+                            publishProgress(String.valueOf(percentage));
+                            output.write(data, 0, readBytes);
+                        }
+
+                        output.flush();
+                        output.close();
+                        input.close();
+                        File p = new File(path[i]);
+                        if(p.exists()){
+                            path[i] = MainActivity.this.getFilesDir() + "/" + FILE_Name[i] + ".c";
+                            Log.d("path created?"+i, path[i]);
+                        }
+                    } catch (Exception e) {
+                        Log.d("Error", e.getMessage());
+                    }
+                }
+            }
+            return path;
+        }
     }
 }
